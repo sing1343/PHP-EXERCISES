149f786e45e259f610dce5739d69ecad7e53e912
sing1343 <59567709+sing1343@users.noreply.github.com>
2020-01-14 12:12:59 -0500

    Create Introduction-to-php.php

---
+++ Introduction-to-php.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+
+
+echo "<h1> Task 1 </h1>";
+
+echo "Welcome ". "to ". "PHP ". "! ";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+
+
+echo "<h1> Task 2 </h1>";
+
+echo "PHP ". "stands ". "for ". '"Hypertext Preprocessor "'. "!";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+
+/* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/
+
+
+echo "<h1> Task 3 </h1>";
+
+echo '<p>PHP stands for "Hypertext Preprocessor"!</p>';
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+
+/* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+*/
+
+
+echo "<h1> Task 4 </h1>";
+
+echo "HTML ". "- ". "for ". "Hypertext ". "Markup ". "Language <br>";
+echo "CSS ". "- ". "Stands ". "for ". "Cascading ". "Stylesheet <br>";
+echo "JS ". "- ". "Stands ". "for ". "JavaScript <br>";
+echo "PHP ". "- ". "Stands ". "for ". "Hypertext ". "Preprocessor <br>";
+echo "SQL ". "- ". "Stands ". "for ". "Structural ". "Query ". "Language <br>";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+
+/* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+*/
+
+
+echo "<h1> Task 5 </h1>";
+
+echo "<ul>
+    <li>HTML Stands for Hypertext Markup Language</li>
+    <li>CSS Stands for Cascading Stylesheet</li>
+    <li>JS Stands for JavaScript</li>
+    <li>PHP Stands for Hypertext Preprocessor</li>
+    <li>SQL Stands for Structural Query Language</li>
+    </ul>"
+
+?>
